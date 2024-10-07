5befc2dac5d40dd9af9d005e50b3dfef5d5fc30b
Surjeet Dhakad <surjeetdhakad8@gmail.com>
2024-10-07 18:24:34 +0530

    deployment

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api=axios.create({
-    baseURL: 'http://localhost:5000/api/'
+    baseURL: 'https://testpro-server-x05k.onrender.com/'
     // headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
 });
 
