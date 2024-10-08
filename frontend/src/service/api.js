f5b1393c687279e31a70dc18436476195cd3ece6
Surjeet Dhakad <surjeetdhakad8@gmail.com>
2024-10-08 17:18:00 +0530

    deployment

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api=axios.create({
-    baseURL: 'https://test-pro-online-test-57jq9yttk-surjeet-dhakads-projects.vercel.app/api/'
+    baseURL: 'https://test-pro-online-test.vercel.app/api/'
     // headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
 });
 
