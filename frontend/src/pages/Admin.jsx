5056323bcf16f562c5a3d34329e15a0cf724f533
Surjeet Dhakad <surjeetdhakad8@gmail.com>
2024-11-05 07:58:01 +0530

    responsive for phone

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -11,7 +11,7 @@ function Admin() {
               <button onClick={(e)=>{e.preventDefault(); return setAdminNav((pre)=>!pre)}} className='text-start lg:hidden font-semibold border-[#01b4dc] pb-4'>Admin Options <FontAwesomeIcon icon={faChevronDown} /></button>
                <div onClick={(e)=>{e.preventDefault(); return setAdminNav((pre)=>!pre)}} className={`w-[100%] lg:w-[20%] h-[100vh] p-4 pt-0 lg:pt-4 lg:border shadow-lg rounded absolute  left-[0] ${AdminNav?"top-[5.5rem] lg:top-[0]":"bottom-[100%]"} transition-all ease-in-out delay-150 duration-200 lg:relative`}>
                       
-                      <ul className=' w-[full] h-[full] flex flex-col gap-2 lg:gap-6 bg-white'>
+                      <ul className=' w-[full] h-[full] flex flex-col gap-2 pb-4 lg:gap-6 bg-white'>
                           <NavLink className={({isActive})=>`${isActive?"text-white bg-[#01b4dc] rounded p-1":""} ps-2`} to="/admin/users"><FontAwesomeIcon icon={faUsers} /> Manage Users</NavLink>
                           <NavLink className={({isActive})=>`${isActive?"text-white bg-[#01b4dc] rounded p-1":""} ps-2`} to="/admin/tests"><FontAwesomeIcon icon={faFilePen} /> Manage Tests</NavLink>
                           <NavLink className={({isActive})=>`${isActive?"text-white bg-[#01b4dc] rounded p-1":""} ps-2`} to="/admin/create"><FontAwesomeIcon icon={faFileCirclePlus} /> Create Test</NavLink>
